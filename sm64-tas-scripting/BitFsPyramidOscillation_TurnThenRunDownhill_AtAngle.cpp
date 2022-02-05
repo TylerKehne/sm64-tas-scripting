3591035c0ade6e00965064f679263750d5095b70
Brian Wheatman <bwheatman@gmail.com>
2022-02-05 10:22:41 -0500

    Very minor cleanups in BitFsPyramidOscillation scripts

--- sm64-tas-scripting/BitFsPyramidOscillation_TurnThenRunDownhill_AtAngle.cpp
+++ sm64-tas-scripting/BitFsPyramidOscillation_TurnThenRunDownhill_AtAngle.cpp
@@ -13,9 +13,8 @@ bool BitFsPyramidOscillation_TurnThenRunDownhill_AtAngle::verification()
 	if (!floorObject)
 		return false;
 
-	const BehaviorScript* behavior = floorObject->behavior;
 	const BehaviorScript* pyramidBehavior = (const BehaviorScript*)(game->addr("bhvBitfsTiltingInvertedPyramid"));
-	if (behavior != pyramidBehavior)
+	if (floorObject->behavior != pyramidBehavior)
 		return false;
 
 	//Check that Mario can enter walking action
@@ -132,7 +131,7 @@ bool BitFsPyramidOscillation_TurnThenRunDownhill_AtAngle::execution()
 
 bool BitFsPyramidOscillation_TurnThenRunDownhill_AtAngle::validation()
 {
-	if (!BaseStatus.m64Diff.frames.size())
+	if (BaseStatus.m64Diff.frames.empty())
 		return false;
 
 	if (CustomStatus.finalXzSum < _minXzSum - 0.00001)
