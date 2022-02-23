dc0fbb60c37a675bc7fb54e7549b625de28b4f09
jgcodes2020 <jackyguo18@hotmail.com>
2022-02-22 23:11:16 -0500

    Remove NULL define from UltraTypes

--- sm64-tas-scripting/UltraTypes.hpp
+++ sm64-tas-scripting/UltraTypes.hpp
@@ -1,13 +1,7 @@
 #pragma once
-#ifndef _ULTRA64_TYPES_H_
-	#define _ULTRA64_TYPES_H_
 
-	#ifndef NULL
-		#define NULL (void*) 0
-	#endif
-
-	#define TRUE 1
-	#define FALSE 0
+#include <stddef.h>
+#include <stdint.h>
 
 typedef signed char s8;
 typedef unsigned char u8;
@@ -30,16 +24,4 @@ typedef volatile s64 vs64;
 typedef float f32;
 typedef double f64;
 
-	#ifdef TARGET_N64
-typedef u32 size_t;
-typedef s32 ssize_t;
-typedef u32 uintptr_t;
-typedef s32 intptr_t;
-typedef s32 ptrdiff_t;
-	#else
-		#include <stddef.h>
-		#include <stdint.h>
 typedef ptrdiff_t ssize_t;
-	#endif
-
-#endif
