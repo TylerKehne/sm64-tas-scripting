cf974f68acac319aeb876735197fca2ee355cdd4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 06:43:18 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/conan_installer.py
+++ scripts/conan_installer.py
@@ -17,7 +17,29 @@ def download(package_name, tar_url):
 		conn = request.urlopen(req)
 		try:
 			with tarfile.open(fileobj=conn, mode="r:gz") as tar:
-				tar.extractall(dir)
+	
+	import os
+	
+	def is_within_directory(directory, target):
+		
+		abs_directory = os.path.abspath(directory)
+		abs_target = os.path.abspath(target)
+	
+		prefix = os.path.commonprefix([abs_directory, abs_target])
+		
+		return prefix == abs_directory
+	
+	def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+	
+		for member in tar.getmembers():
+			member_path = os.path.join(path, member.name)
+			if not is_within_directory(path, member_path):
+				raise Exception("Attempted Path Traversal in Tar File")
+	
+		tar.extractall(path, members, numeric_owner=numeric_owner) 
+		
+	
+	safe_extract(tar, dir)
 		finally:
 			conn.close()
 		
